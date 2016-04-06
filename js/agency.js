9651faa509cde83d5e4d099512b9e45b973494bd
Mathias Quintero <mathias.quintero@tum.de>
2016-04-06 19:59:08 +0200

    minor changes and changing profit image in about section

--- js/agency.js
+++ js/agency.js
@@ -5,11 +5,10 @@
  */
 
  $.fn.extend({
-     animateCss: function (animationName, callback) {
+     animateCss: function (animationName) {
          var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
          $(this).addClass('animated ' + animationName).one(animationEnd, function() {
              $(this).removeClass('animated ' + animationName);
-             callback();
          });
      }
  });
